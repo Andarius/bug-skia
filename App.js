222ade835d38f511002f06a28eb03f54c115bc8d
Julien Brayere <julien.brayere@obitrain.com>
2023-10-02 15:49:43 +0200

    chore: adding repro toCmds

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
-import { Canvas, Circle, Group } from "@shopify/react-native-skia";
+import { Canvas, Circle, Group, Skia } from "@shopify/react-native-skia";
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
   const size = 256;
   const r = size * 0.33;
+
+  console.log(
+    console.log(
+      Skia.Path.MakeFromSVGString(
+        "M0,35L60,140L120,105L180,70L240,35L300,0L360,105"
+      ).toCmds()
+    )
+  );
+
   return (
-    <Canvas style={{ flex: 1 }}>
-      <Group blendMode="multiply">
-        <Circle cx={r} cy={r} r={r} color="cyan" />
-        <Circle cx={size - r} cy={r} r={r} color="magenta" />
-        <Circle cx={size / 2} cy={size - r} r={r} color="yellow" />
-      </Group>
-    </Canvas>
+    <View style={styles.container}>
+      <Canvas style={{ flex: 1 }}>
+        <Group blendMode="multiply">
+          <Circle cx={r} cy={r} r={r} color="cyan" />
+          <Circle cx={size - r} cy={r} r={r} color="magenta" />
+          <Circle cx={size / 2} cy={size - r} r={r} color="yellow" />
+        </Group>
+      </Canvas>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+  },
+});
